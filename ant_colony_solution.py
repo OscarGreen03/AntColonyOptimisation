addb1cb94465a9342c1aee6763328a193b01fff1
OscarGreen03 <greenorh@icloud.com>
2024-10-22 21:09:49 +0100

    Initial commit

---
+++ ant_colony_solution.py
@@ -0,0 +1,198 @@
+import math
+import random
+from bisect import bisect_left
+import numpy as np
+import graph_generation
+import matplotlib.pyplot as plt
+
+
+class AntColony(graph_generation.Graph):
+    def __init__(self):
+        super().__init__()
+
+    def evaporate(self, rate):
+        for node in self.graph:
+            for edge in self.graph[node]:
+                self.graph[node][edge] *= rate
+
+
+
+
+
+
+    def get_next_step(self, node, alpha=1, beta=1):
+        # alpha is pheromone**alpha
+        current_edges = self.graph[node]
+        edge_list = list(current_edges.keys())
+        if "END" in edge_list:
+            return "END"
+        if "START" in edge_list:
+            edge_list.remove("START")
+        if node == "START":
+            node = (-1, -1,)
+        valid_edges = [edge for edge in edge_list if isinstance(edge, tuple) and (edge[1] > node[1])]
+        ## all edges where is a tuple and item (index 1) is larger than the previous item
+        ## this means it can only move foward
+
+
+
+        '''weights = [current_edges[edge] for edge in valid_edges]
+        cum_weights = []
+        total = 0
+        for weight in weights:
+            total += weight
+            cum_weights.append(total)
+        choice = random.uniform(0, cum_weights[-1])
+        index = bisect_left(cum_weights, choice)
+        return valid_edges[index]'''
+
+
+        #Is this faster??
+        weights = np.array([current_edges[edge] for edge in valid_edges])
+        cumulative_weights = np.cumsum(weights)
+        choice = random.uniform(0, cumulative_weights[-1])
+        index = np.searchsorted(cumulative_weights, choice)
+        return valid_edges[index]
+
+
+
+
+        '''
+        sum_weight = sum([current_edges[edge] for edge in valid_edges])
+        choice = random.uniform(0, sum_weight)
+        c = 0
+        for edge in valid_edges:
+
+            c = c + current_edges[edge]
+            if c >= choice:
+                return edge
+        
+        raise Exception("No Node Found")
+        '''
+
+    def produce_path(self):
+
+        current_edges = {}
+        visited_edges = {}
+        current_node = "START"
+        path = ["START"]
+        # if "START" in self.graph.keys():
+        #    current_edges = self.graph["START"]
+        #    print("Current Edges: " + str(current_edges))
+        # else:
+        #   print("START not found??")
+        #   print("Graph Keys: " + str(self.graph.keys()))
+
+        while current_node != "END":
+            next_step = self.get_next_step(current_node)
+            path.append(next_step)
+            current_node = next_step
+        # print("Path Completed")
+
+        return path
+
+    def evaluate_fitness(self, path, items, bin_count):
+
+        bins = [0 for _ in range(bin_count)]
+
+        for i in range(len(path)):
+            # for each item, add to bin
+            if isinstance(path[i], tuple):
+                # print("Adding item " + str(i) + ", "+ str(items[i - 1]) + "kg, to bin " + str(path[i][0]))
+                bins[path[i][0]] += items[i - 1]
+        # print("Fitness: " + str(math.sqrt(max(bins)**2 - min(bins)**2)))
+        return max(bins) - min(bins), max(bins), min(bins)
+        # return [math.sqrt(max(bins)**2 - min(bins)**2), max(bins), min(bins)]
+
+    def generate_paths(self, n):
+        pass
+
+    def update_pheremones(self, path, fitness, weighting=0.001, global_weight=1, upper_bound=0, lower_bound=0):
+        # local weight is how much to update path pheromone by score
+        added_pheremone = 100 / fitness if fitness > 0 else 100
+
+        for i in range(len(path) - 1):
+            self.graph[path[i]][path[i + 1]] += added_pheremone
+            # print(100/fitness)
+            # score = ((fitness-lower_bound)/(upper_bound-lower_bound)) * global_weight
+            # self.graph[path[i]][path[i+1]] += (1 - score) * weighting
+            # self.graph[path[i]][path[i + 1]] = min(self.graph[path[i]][path[i+1]], 1)
+
+
+def BPP(bin_count, items, population=10, max_generations=50, e=0.8):
+    path_history = []
+    g = graph_generation.produce_bin_graph(bin_count, len(items), lower_bound=0, upper_bound=1)
+    colony = AntColony()
+    colony.set_graph(g)
+    # colony.visualise_graph(show=True)
+
+    # generate a bunch of valid paths (population : n)
+    # Evaluate fitness of each
+    # Adjust pheramones for the path based on fitness
+    # evaporate path
+    # repeat until termination conditions met
+    best_path = None
+    best_fitness = float('inf')
+    for i in range(max_generations):
+        # colony.print_max_min_weights()
+        paths = [[colony.produce_path()] for _ in range(population)]
+
+        for path in paths:
+            fitness = colony.evaluate_fitness(path[0], items, bin_count)
+            # colony.update_pheremones(path[0])
+            # path.extend(colony.evaluate_fitness(path[0], items, bin_count)) # may need to ajust
+            colony.update_pheremones(path[0], fitness[0])
+            if fitness[0] < best_fitness:
+                best_path, best_fitness = path[0], fitness[0]
+        # for path in paths:
+        # print(path)
+        # for path in paths:
+        #    if path[1] < best_fitness:
+        #        best_path, best_fitness = path[0], path[1]
+        #    colony.update_pheremones(path[0], path[1], weighting=1,global_weight=1)
+
+        colony.evaporate(e)
+        #path_history.append(colony.as_nx_graph())
+        # colony.visualise_graph(i)
+        if i % 100 == 0:
+            print(i)
+
+    # colony.visualise_graph(show=True)
+    # print(colony.graph)
+    # print(paths[-1])
+    # print("Best path is: " + str(best_path))
+    # print("Fitness: " + str(best_fitness))
+    #colony.visualise_graph_with_path(best_path, show=True)
+    # graph_generation.produce_animation(path_history)
+    return best_path, best_fitness
+
+
+# items_list = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 50]
+# [32, 12, 8 , 4, 13, 18, 36, 29, 42]
+# print(BPP(50,items_list , 10, 1000, e=0.6))
+
+tests = [
+    (100, 0.9),
+    (100, 0.6),
+    (10, 0.9),
+    (10, 0.6)
+]
+items_list1 = list(range(1, 501))
+items_list2 = [(i ** 2) / 2 for i in range(1, 501)]
+for test in tests:
+    print("------------------")
+    print(f"p = {test[0]}" + " " * (4 - len(str(test[0]))) + f"with e = {test[1]}")
+    best_path, best_fitness = BPP(10, items_list1, test[0], 1000, e=test[1])
+    # print("Best path is: " + str(best_path))
+    print("Fitness: " + str(best_fitness))
+'''
+print("------------------")
+print("Second Set:")
+for test in tests:
+    print("------------------")
+    print(f"p = {test[0]}" + " " * (5 - len(str(test[0]))) + f"with e = {test[1]}")
+    best_path, best_fitness = BPP(50, items_list2, test[0], 10000, e=test[1])
+    # print("Best path is: " + str(best_path))
+    print("Fitness: " + str(best_fitness))
+print("------------------")
+'''
---
+++ graph_generation.py
@@ -0,0 +1,310 @@
+from random import randint, uniform
+
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from matplotlib.lines import Line2D
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def print_max_min_weights(self):
+        maximum = 0
+        minimum = float(9999999)
+        for node in self.graph.keys():
+            for edge in self.graph[node].keys():
+                weight = self.graph[node][edge]
+                if weight > maximum:
+                    maximum = weight
+                    max_edge = f"{node} {edge}"
+                elif weight < minimum:
+                    minimum = weight
+                    min_edge = f"{node} {edge}"
+        print("Max: " + str(maximum) + " " * (30 - len(str(maximum))) + "Min: " + str(minimum))
+        print("Max node = " + str(max_edge))
+        print("Min node = " + str(min_edge))
+
+    def set_graph(self, graph):
+        if isinstance(graph, Graph):
+            self.graph = graph.graph
+        elif isinstance(graph, dict):
+            self.graph = graph
+        else:
+            print("Type: " + str(type(graph)) + " is not supported. Please use a dict or a Graph")
+
+    def add_node(self, node):
+        if node in self.graph:
+            raise Exception(f"Node {node} already exists")
+        self.graph[node] = {} ## dict of directions and weights
+
+    def add_edge(self, n1, n2, weight):
+        if n1 not in self.graph:
+            raise Exception(f"Node {n1} not in graph")
+        if n2 not in self.graph:
+            raise Exception(f"Node {n2} not in graph")
+        self.graph[n1][n2] = weight
+        #self.graph[n2][n1] = weight
+
+    def edit_weight(self, n1, n2, weight):
+        if n1 not in self.graph:
+            raise Exception(f"Node {n1} not in graph")
+        if n2 not in self.graph:
+            raise Exception(f"Node {n2} not in graph")
+
+
+
+        self.graph[n1][n2] = weight
+        self.graph[n2][n1] = weight
+
+    def get_nodes(self):
+        return self.graph.keys()
+
+    def get_node_connections(self, node):
+        return self.graph[node].keys()
+
+    def __str__(self):
+        return str(self.graph)
+
+    def as_nx_graph(self):
+        G = nx.Graph()
+        node_mapping = {}
+        for node in self.graph:
+            G.add_node(node)
+            for edge in self.graph[node]:
+                G.add_edge(node, edge, weight=self.graph[node][edge])
+
+        numerical_nodes = []
+        for node in G.nodes():
+            if isinstance(node, tuple) and len(node) == 2:
+                try:
+                    j = float(node[0])
+                    i = float(node[1])
+                    numerical_nodes.append((i, j))
+                except ValueError:
+                    pass  # Skip nodes where conversion fails
+
+        if numerical_nodes:
+            is_list, js_list = zip(*numerical_nodes)
+            min_i = min(is_list)
+            max_i = max(is_list)
+            min_j = min(js_list)
+            max_j = max(js_list)
+            avg_j = (min_j + max_j) / 2
+        else:
+            # Default values in case there are no numerical nodes
+            min_i = 0
+            max_i = 0
+            avg_j = 0
+
+        # Create positions
+        pos = {}
+        for node in G.nodes():
+            if isinstance(node, tuple) and len(node) == 2:
+                try:
+                    j = float(node[0])
+                    i = float(node[1])
+                    pos[node] = (i, j)
+                except ValueError:
+                    print(f"Node {node} has non-numeric identifiers.")
+                    continue
+            elif node == "START":
+                # Position "START" node to the left of the min_i
+                pos[node] = (min_i - 1, avg_j)
+            elif node == "END":
+                # Position "END" node to the right of the max_i
+                pos[node] = (max_i + 1, avg_j)
+            else:
+                # Position other non-numeric nodes at a default location
+                pos[node] = (max_i + 2, avg_j)
+        # pos = {node: (y, x) for node, (x, y) in pos.items()}
+        edge_weights = nx.get_edge_attributes(G, 'weight')
+        weights = [edge_weights[edge] for edge in G.edges()]
+        return G, pos, weights
+
+    def visualise_graph(self, graph_name=None, show=False):
+
+        G = nx.Graph()
+        node_mapping = {}
+        for node in self.graph:
+            G.add_node(node)
+            for edge in self.graph[node]:
+
+                G.add_edge(node, edge, weight=self.graph[node][edge])
+
+        numerical_nodes = []
+        for node in G.nodes():
+            if isinstance(node, tuple) and len(node) == 2:
+                try:
+                    j = float(node[0])
+                    i = float(node[1])
+                    numerical_nodes.append((i, j))
+                except ValueError:
+                    pass  # Skip nodes where conversion fails ( Start and End Nodes)
+
+        if numerical_nodes:
+            is_list, js_list = zip(*numerical_nodes)
+            min_i = min(is_list)
+            max_i = max(is_list)
+            min_j = min(js_list)
+            max_j = max(js_list)
+            avg_j = (min_j + max_j) / 2
+        else:
+            raise Exception("No Numerical Nodes")
+
+            #min_i = 0
+            #max_i = 0
+            #avg_j = 0
+
+        # Create positions
+        pos = {}
+        for node in G.nodes():
+            if isinstance(node, tuple) and len(node) == 2:
+                try:
+                    j = float(node[0])
+                    i = float(node[1])
+                    pos[node] = (i, j)
+                except ValueError:
+                    print(f"Node {node} has non-numeric identifiers.")
+                    continue
+            elif node == "START":
+                # Position "START" node to the left of the min_i
+                pos[node] = (min_i - 1, avg_j)
+            elif node == "END":
+                # Position "END" node to the right of the max_i
+                pos[node] = (max_i + 1, avg_j)
+            else:
+                # Position other non-numeric nodes at a default location
+                pos[node] = (max_i + 2, avg_j)
+        #pos = {node: (y, x) for node, (x, y) in pos.items()}
+        #plt.figure(figsize=(30, 20))
+        # Draw the graph with specified positions and labels
+
+        edge_weights = nx.get_edge_attributes(G, 'weight')
+        weights = [edge_weights[edge] for edge in G.edges()]
+
+        fig, ax = plt.subplots(dpi=600)
+        nx.draw_networkx(G, pos, with_labels=True, node_size=50, font_size=4, width=weights, ax=ax) # width=1
+
+        legend_elements = [
+            Line2D([0], [0], marker='o', color='w', label='Node',
+                   markerfacecolor='lightblue', markersize=10),
+            Line2D([0], [0], color='gray', lw=2, label='Edge')
+        ]
+
+        # Add the legend to the plot
+        plt.legend(handles=legend_elements, loc='upper right')
+        #ax.set_xlabel('Object')
+        #ax.set_ylabel('Bin')
+        #ax.set_title(graph_name)
+
+        plt.xlabel('Object')
+        plt.ylabel('Bin')
+        plt.title(f'Graph Visualization {" of " + str(graph_name) if graph_name is not None else "A"}')
+        #plt.grid(True)
+        #plt.axis('equal')  # Ensure equal scaling on both axes#
+        if show:
+            plt.show()
+        plt.close(fig)
+        return fig, ax
+
+
+    def visualise_graph_with_path(self, path, graph_name=None, show=False):
+        G, pos, weights = self.as_nx_graph()
+        # Extract the edges in the path
+        path_edges = list(zip(path, path[1:]))
+
+        # Create edge colors and widths
+        edge_colors = []
+        edge_widths = []
+        for edge in G.edges():
+            if edge in path_edges or (edge[1], edge[0]) in path_edges:  # For undirected graphs
+                edge_colors.append('red')
+                edge_widths.append(2.5)
+            else:
+                edge_colors.append('gray')
+                edge_widths.append(0.5)
+
+        # Create node colors
+        node_colors = []
+        for node in G.nodes():
+            if node in path:
+                node_colors.append('red')
+            else:
+                node_colors.append('lightblue')
+
+        fig, ax = plt.subplots(dpi=600)
+        nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=50, ax=ax)
+        nx.draw_networkx_labels(G, pos, font_size=4, ax=ax)
+        nx.draw_networkx_edges(G, pos, edge_color=edge_colors, width=edge_widths, ax=ax)
+
+        # Create legend
+        legend_elements = [
+            Line2D([0], [0], marker='o', color='w', label='Node',
+                   markerfacecolor='lightblue', markersize=10),
+            Line2D([0], [0], color='gray', lw=2, label='Edge'),
+            Line2D([0], [0], color='red', lw=2, label='Path Edge'),
+            Line2D([0], [0], marker='o', color='w', label='Path Node',
+                   markerfacecolor='red', markersize=10)
+        ]
+
+        plt.legend(handles=legend_elements, loc='upper right')
+        plt.xlabel('Object')
+        plt.ylabel('Bin')
+        plt.title(f'Graph Visualization {" of " + str(graph_name) if graph_name is not None else "A"}')
+
+        if show:
+            plt.show()
+        plt.close(fig)
+        return fig, ax
+
+def produce_bin_graph(bin_count, object_count, lower_bound=0, upper_bound=1):
+    ## the graph represents the probelem
+    newGraph = Graph()
+
+    ## each turn needs to be represented in the graph.
+    # Move = Put next item in this box
+    # aka: Move to node 3, 1 is put item 1 in box 3
+    #newGraph.add_node("START")
+    for i in range(bin_count):
+        for j in range(object_count):
+            newGraph.add_node((i, j))
+    # cbin current bin
+    for cbin in range(bin_count):
+        for cobject in range(object_count - 1):
+            for new_bin in range(bin_count):
+                # for each node in graph, create connections to each in the next line
+                newGraph.add_edge((cbin, cobject), (new_bin, cobject+1), uniform(lower_bound, upper_bound))
+
+
+                #print("=========")
+
+    newGraph.add_node("START")
+    newGraph.add_node("END")
+    for i in range(bin_count):
+        newGraph.add_edge("START", (i, 0), uniform(lower_bound,upper_bound))
+        newGraph.add_edge((i, object_count-1),"END", uniform(lower_bound,upper_bound))
+
+    return newGraph
+
+
+
+
+def get_next_frame(i, frames, ax):
+    ax.clear() #clear prev frame
+    G, pos, weights = frames[i]
+    nx.draw_networkx(G, pos, with_labels=True, node_size=300, font_size=8, width=(weights), ax=ax)
+    ax.set_title(f'Graph Visualization of {i}')
+    return ax.artists
+
+def produce_animation(graphs):
+
+    #fig1, ax1 = graphs[0].visualise_graph()
+    #G, pos, weights = graphs[0]
+    fig1, ax1 = plt.subplots(dpi=300)
+
+    #frames = [graphs[i].as_nx_graph() for i in range(1, len(graphs))]
+    ani = animation.FuncAnimation(fig1, get_next_frame, fargs=(graphs, ax1), frames=len(graphs))
+    ani.save("testani.mp4", writer='ffmpeg', fps=5)
+
+
