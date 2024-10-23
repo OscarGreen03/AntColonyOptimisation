2c33b5c940abe14085ff9165e4099c588bc00ca8
OscarGreen03 <greenorh@icloud.com>
2024-10-23 16:24:08 +0100

    Attempting to optimise code and print results more clearly. Added ability to use the same colony for multiple runs. Added evaporation (oops) BPP now returns only best fitness of last generation.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from multiprocessing import Pool
 from random import uniform
 from bisect import bisect_left
 import numpy as np
+from tabulate import tabulate
 class AntColony:
     def __init__(self):
         self.start = []
@@ -24,6 +26,19 @@ class AntColony:
 
     def get_next_step(self, current_item, current_bin):
         if current_bin == "START":
+            weights = self.start
+        elif current_item == self.item_count:
+            return "END"
+        else:
+            weights = self.graph[current_item - 1, current_bin]
+        total_pheromone = np.sum(weights)
+        probabilities = weights / total_pheromone
+        choice = np.random.choice(self.bin_count, p=probabilities)
+        return choice
+
+
+        '''if current_bin == "START":
+            weights = self.start
             cum_weight = np.cumsum(self.start)
             choice = uniform(0, cum_weight[-1])
             item_index = np.searchsorted(cum_weight, choice)
@@ -36,7 +51,7 @@ class AntColony:
         cum_weights = np.cumsum(weights)
         choice = uniform(0, cum_weights[-1])
         item_index = np.searchsorted(cum_weights, choice)
-        return item_index
+        return item_index'''
 
 
 
@@ -74,13 +89,16 @@ class AntColony:
 
 
 
-def BPP(bin_count, items, iterations, p, e):
-    colony = AntColony()
-    colony.set_bin_graph(bin_count, len(items))
+def BPP(bin_count, items, iterations, p, e, colony=None):
+    if colony is None:
+        colony = AntColony()
+        colony.set_bin_graph(bin_count, len(items))
+
     best_path = []
     best_fitness = float('inf')
 
     for i in range(iterations):
+
         paths = [colony.get_new_path() for _ in range(p)]
         for path in paths:
             fitness = colony.evaluate_fitness(path, items)
@@ -89,8 +107,25 @@ def BPP(bin_count, items, iterations, p, e):
             if fitness < best_fitness:
                 best_fitness = fitness
                 best_path = path
-                print(f"New best solution {i} found with fitness: {fitness}")
-    return best_path, best_fitness
+                #print(f"Generation {i} found with fitness: {fitness}")
+        colony.evaporate(e)
+
+    ## final generation of paths
+    best_path = []
+    best_fitness = float('inf')
+    for path in paths:
+        fitness = colony.evaluate_fitness(path, items)
+        if fitness < best_fitness:
+            best_fitness = fitness
+            best_path = path
+    return best_fitness
+
+
+
+
+
+
+
 # main array containing each column of the problem as an item in the array
 # each item in this main array is an array of the bins and their weights to each other bin
 # this array cont
@@ -108,25 +143,51 @@ def BPP(bin_count, items, iterations, p, e):
 
 #BPP(10, [20, 30, 40, 30, 20, 10, 20, 30, 14, 64, 12, 34, 23, 63, 36, 28], 1000, 100, 0.9)
 
+if __name__ == '__main__':
+    tests = [
+        (100, 0.9),
+        (100, 0.6),
+        (10, 0.9),
+        (10, 0.6)
+    ]
+    items_1 = list(range(1, 501))
+    items_2 = [(i ** 2) / 2 for i in range(1, 501)]
 
-tests = [
-    (100, 0.9),
-    (100, 0.6),
-    (10, 0.9),
-    (10, 0.6)
-]
-items_list1 = list(range(1, 501))
-items_list2 = [(i ** 2) / 2 for i in range(1, 501)]
-for test in tests:
-    print("------------------")
-    print(f"p = {test[0]}" + " " * (4 - len(str(test[0]))) + f"with e = {test[1]}")
-    best_path, best_fitness = BPP(10, items_list2, 10000, test[0], test[1])
-    # print("Best path is: " + str(best_path))
-    print("Fitness: " + str(best_fitness))
+    for bin_count in [10, 50]:
+        for test in tests:
+            print("------------------")
+            print(f"p = {test[0]}" + " " * (4 - len(str(test[0]))) + f"with e = {test[1]}")
+            iterations = 10000//test[0]
+            results = []
 
-for test in tests:
-    print("------------------")
-    print(f"p = {test[0]}" + " " * (4 - len(str(test[0]))) + f"with e = {test[1]}")
-    best_path, best_fitness = BPP(10, items_list2, 10000, test[0], test[1])
-    # print("Best path is: " + str(best_path))
-    print("Fitness: " + str(best_fitness))
+            #colony = AntColony()
+            #colony.set_bin_graph(10, len(items_1))
+
+            for i in range(5):
+                print(f"{i + 1}", end="")
+
+                results.append(BPP(bin_count, items_1, iterations, test[0], test[1], colony=None))
+
+            print(f"\nResults b={bin_count}, p={test[0]}, e={test[1]}")
+            for result in results:
+                print(result)
+            print("------------------")
+
+
+
+    for test in tests:
+        print("------------------")
+        print(f"p = {test[0]}" + " " * (4 - len(str(test[0]))) + f"with e = {test[1]}")
+
+        iterations = 10000//test[0]
+
+        best_fitness, best_path  = BPP(10, items_1, iterations, test[0], test[1])
+        # print("Best path is: " + str(best_path))
+        print("Fitness: " + str(best_fitness))
+
+    for test in tests:
+        print("------------------")
+        print(f"p = {test[0]}" + " " * (4 - len(str(test[0]))) + f"with e = {test[1]}")
+        best_fitness, best_path = BPP(10, items_2, 10000, test[0], test[1])
+        # print("Best path is: " + str(best_path))
+        print("Fitness: " + str(best_fitness))
